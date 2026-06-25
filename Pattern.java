13aa1a36040e92a545e52a0d61d20762f8166fe1
rajasree-m006 <rajasree1803@gmail.com>
2026-06-25 15:42:39 +0530

    seventh day

--- Pattern.java
+++ Pattern.java
@@ -4,10 +4,10 @@ public class Pattern {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for(int i=0;i<=n-i;i++){
-            for(int j=1;j<=2*i;j++){
+        for(int i=1;i<=n-i;i++){
+            for(int j=1;j<=2*i-1;j++){
                 System.out.print("*"+" ");
-            }System.out.println();
+            }System.out.println(" ");
         }
         
     }
---
+++ Reverses.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Reverses {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int n =in.nextInt();
+        int[] nums =new int[n];
+        for (int i=0;i<=n;i++){
+            nums[i]=in.nextInt();
+        }
+        for(int i=0;i<=n/2;i++ ){
+            int temp =nums[i];
+            nums[i]=nums[n-1-i];
+            nums[n-1-i]=temp;
+        }
+    }
+    
+}
